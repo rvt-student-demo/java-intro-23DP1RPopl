b609dc9c054333805572e0a1571b3720ec72c71e
23DP1RPopl <166118027+23DP1RPopl@users.noreply.github.com>
2024-09-17 11:06:29 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,19 +10,34 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give the first number:");
+        System.out.println("Give points [0-100]: ");
         int number = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        int number2 = Integer.valueOf(scanner.nextLine());
+
         
-        if (number > number2) {
-            System.out.println("Greater number is: " + number );
+        if (number < 0) {
+            System.out.println("impossible!" );
         }
-        else if (number < number2) {
-            System.out.println("Greater number is: " + number2 );
+        else if (number > -1 && number < 50 ) {
+            System.out.println("failed");
         }
-        else {
-            System.out.println("The numbers are equal!" );
+        else if (number > 49 && number < 60 ) {
+            System.out.println("1");
+        }
+        else if (number > 59 && number < 70 ) {
+            System.out.println("2");
+        }
+        else if (number > 69 && number < 80 ) {
+            System.out.println("3");
+        }
+        else if (number > 79 && number < 90 ) {
+            System.out.println("4");
+        }
+        else if (number > 89 && number < 101 ) {
+            System.out.println("1");
+        }
+        else if (number > 100) {
+            
+            System.out.println("incredible!" );
         }
         
        
