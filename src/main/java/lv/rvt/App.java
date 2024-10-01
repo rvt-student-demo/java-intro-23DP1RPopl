1054fa55a2efb54227a2af33e840c76f06c68d04
23DP1RPopl <166118027+23DP1RPopl@users.noreply.github.com>
2024-10-01 07:45:56 +0000

    Reprint

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,33 +10,17 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int sum = 0;
-        int record = 0;
-        System.out.println("Give numbers:");
-        int pos = 0;
-        int neg = 0;
+        System.out.println("How many times? ");
+        int numberFromUser = Integer.valueOf(scanner.nextLine());
         while (true) {
-            int numberFromUser = Integer.valueOf(scanner.nextLine());
-
-            if (numberFromUser == -1 ) {
+            if (numberFromUser == 0){
                 break;
             }
-            if (numberFromUser % 2 == 0) {
-                pos += 1;
-            } else {
-                neg += 1;
-            }
-            sum += numberFromUser;
-            record += 1;
-
+            printText();
+            numberFromUser = numberFromUser - 1;
         }
-        int average = sum / record;
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: " + record);
-        System.out.println("Average: " + average);
-        System.out.println("Even: " + pos);
-        System.out.println("Odd: " + neg);
-
+    }
+    public static void printText() {
+     System.out.println("In a hole in the ground there lived a method");       
     }
 }
