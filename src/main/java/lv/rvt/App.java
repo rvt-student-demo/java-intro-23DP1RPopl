3fd9223fbb1cd3431c4f516e3d5a8d99e6e71896
23DP1RPopl <166118027+23DP1RPopl@users.noreply.github.com>
2024-10-08 10:00:47 +0000

    Third element

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,47 +3,25 @@ package lv.rvt;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 
 public class App 
 {
-    public static void printSpaces(int number){
-        for (int i = 0; i < number;i++){
-            System.out.print(" ");
-        }
-    }
-    public static void printStars(int number) {
-        for (int i = 0; i < number; i++){
-            System.out.print("*");
-        }
-    }
-
-    public static void printTriangleTwo(int size){
-     for (int si = 1; si < size + 1 ; si++){
-        printSpaces(size - si);
-        printStars(si);
-        System.out.println("");
-     } 
- }
- public static void christmasTree(int height){
-    for (int he = 1; he < height + 1 ; he++){
-        printSpaces(height - he);
-        printStars(he);
-        printStars(he-1);
-        System.out.println("");
-     } 
-     printSpaces(height - 2);
-     printStars(3);
-     System.out.println("");
-     printSpaces(height - 2);
-     printStars(3);
-     System.out.println("");
-     
- }
     public static void main(String[] args) {
-        printTriangleTwo(4);
-        christmasTree(4);
-        christmasTree(10);
+        ArrayList<String> wordList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int a = 1;
+        while(a == 1){
+            String vard = scanner.nextLine();
+            wordList.add(vard);
+            if (vard == ""){
+                a = 0;
+            }  
+        }
+
+    
+        System.out.println(wordList.get(2));
     }
 
 }
