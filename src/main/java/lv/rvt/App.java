f7b31f1aaa6336d5a9b8b4d970ed9d302a681f9d
23DP1RPopl <166118027+23DP1RPopl@users.noreply.github.com>
2024-10-08 06:22:27 +0000

    Star sign (4 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,17 +7,35 @@ import java.util.Scanner;
 
 public class App 
 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number1 = Integer.valueOf(scanner.nextLine());
-        int number2 = Integer.valueOf(scanner.nextLine());
-        divisibleByThreeInRange(number1, number2);
-    }
-    public static void  divisibleByThreeInRange(int beginning, int end) {
-     for (int i = beginning; i < end + 1; i++){
-        if (i % 3 == 0) {
-            System.out.println(i);
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++){
+            System.out.print("*");
         }
-     } 
+        
+        System.out.println("");
+    }
+    public static void printSquare(int size) {
+        for (int i = 0; i < size; i++){
+            printStars(size);
+        }
+    }
+    public static void printRectangle(int width, int height){
+        for (int h = 0 ; h < height; h++){
+            printStars(width);
+            
+        }
+    }
+    public static void printTriangle(int size){
+        for (int i = 0 ; i < size ; i++){
+            printStars(i + 1);
+        }
+    }
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
+        printSquare(4);
+        printRectangle(17, 3);
+        printTriangle(4);
     }
 }
