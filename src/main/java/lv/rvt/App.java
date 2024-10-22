034229717d0af9700677d6b248751a9142f9f6db
23DP1RPopl <166118027+23DP1RPopl@users.noreply.github.com>
2024-10-22 07:52:19 +0000

    Print in range

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,26 +6,29 @@ import java.util.*;
 
 
 public class App 
-{
+{      
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit){
+        for (Integer i : numbers) {
+            if (i >= lowerLimit && i <= upperLimit){
+                System.out.println(i); 
+            } 
+        }
+    }
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        int a = 1;
-        while(a == 1){
-            int number = Integer.valueOf(scanner.nextLine());
-            numbers.add(number);
-            if (number == -1){
-                a = 0;
-            }  
-        }
-        System.out.print("From where? ");
-        int number1 = Integer.valueOf(scanner.nextLine());
-        System.out.print("To where? ");
-        int number2 = Integer.valueOf(scanner.nextLine());
-        for (int i = number1; i <= number2; i++){
-            int number = numbers.get(i);
-            System.out.println(number);
-        }
-    }
+       
+        numbers.add(3);
+        numbers.add(2);
+        numbers.add(6);
+        numbers.add(-1);
+        numbers.add(5);
+        numbers.add(1);
+        
+        System.out.println("The numbers in the range [0, 5]");
+        printNumbersInRange(numbers, 0, 5);
 
+        System.out.println("The numbers in the range [3, 10]");
+        printNumbersInRange(numbers, 3, 10);
+        }
 }
