b4157200af421f4bd7abe7ab2e4e4a87d27d4f41
23DP1RPopl <166118027+23DP1RPopl@users.noreply.github.com>
2025-02-12 07:48:50 +0000

    Interface In A Box (start)

---
+++ src/main/java/lv/rvt/semestris2/Book.java
@@ -0,0 +1,13 @@
+package lv.rvt.semestris2;
+
+public class Book {
+    private String author;
+    private String name;
+    private double bookweight;
+    public Book(String author, String name, double bookweight){
+        this.name = name;
+        this.author = author;
+        this.bookweight = bookweight;   
+    }
+
+}
---
+++ src/main/java/lv/rvt/semestris2/CD.java
@@ -0,0 +1,10 @@
+package lv.rvt.semestris2;
+
+public class CD {
+    private String artist;
+    private String name;
+    private Integer year;
+    public CD(String artist, String name, Integer year){
+        
+    }
+}
